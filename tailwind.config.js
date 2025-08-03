93c47ea33ec42083ed44d51d0e309b306fda4d99
AyumuKawahara <ayumu_note@outlook.jp>
2025-08-03 21:44:50 +0900

    babel.config.js、metro.config.js、tailwind.config.js、nativewind-env.d.ts、global.cssを新規作成し、tsconfig.jsonにnativewind-env.d.tsを追加しました。また、_layout.tsxでglobal.cssをインポートし、index.tsxのコードを簡略化しました。

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./features/**/*.{js,ts,jsx,tsx}",
+		"./layouts/**/*.{js,ts,jsx,tsx}",
+	],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
