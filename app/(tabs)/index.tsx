5341f09f404a3948db8769492b1ffc2ed331938e
AyumuKawahara <ayumu_note@outlook.jp>
2025-08-07 16:02:07 +0900

    babel.config.jsにSQLファイルのインラインインポート用プラグインを追加し、metro.config.jsでSQLファイルの拡張子を解決するように設定を更新しました。また、新たにdrizzle.config.tsを追加し、SQLite用の設定を定義しました。package.jsonとpackage-lock.jsonにdrizzle-ormおよび関連パッケージを追加しました。最後に、アプリのデータベース接続を設定するためにindex.tsxを更新しました。

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,10 @@
 import { RootPage } from "@/features/root";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite";
+
+const expo = openDatabaseSync("ultra-n-back.db", {
+  enableChangeListener: true,
+});
+export const db = drizzle(expo);
 
 export default RootPage;
