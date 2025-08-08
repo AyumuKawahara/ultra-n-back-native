da56595e0e5df784acc3874a5b4d49b6a568e481
AyumuKawahara <ayumu_note@outlook.jp>
2025-08-08 12:48:48 +0900

    新しいBGMファイル（bgm.mp3）を追加しました。

--- layouts/root/index.tsx
+++ layouts/root/index.tsx
@@ -1,3 +1,4 @@
+import { BgmProvider } from "@/providers/bgm";
 import { defaultConfig } from "@tamagui/config/v4";
 import { createTamagui, TamaguiProvider } from "@tamagui/core";
 import { PortalProvider } from "@tamagui/portal";
@@ -16,70 +17,72 @@ export const RootLayout = () => {
   return (
     <TamaguiProvider config={config}>
       <PortalProvider>
-        <StatusBar style="light" />
-        <Stack>
-          <Stack.Screen
-            name="(tabs)"
-            options={{
-              title: "ホーム",
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="setup/index"
-            options={{
-              title: "ゲーム設定",
-              headerStyle: { backgroundColor: "#1A1D21" },
-              headerTintColor: "#1E90FF",
-            }}
-          />
-          <Stack.Screen name="play/index" options={{ headerShown: false }} />
-          <Stack.Screen name="result/index" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="how-to-play/index"
-            options={{
-              title: "あそびかた",
-              headerStyle: { backgroundColor: "#1A1D21" },
-              headerTintColor: "#1E90FF",
-            }}
-          />
-          <Stack.Screen
-            name="others/volume/index"
-            options={{
-              title: "音量設定",
-              headerStyle: { backgroundColor: "#1A1D21" },
-              headerTintColor: "#1E90FF",
-              headerBackTitle: "戻る",
-            }}
-          />
-          <Stack.Screen
-            name="others/plans/index"
-            options={{
-              title: "アップグレード",
-              headerStyle: { backgroundColor: "#1A1D21" },
-              headerTintColor: "#1E90FF",
-              headerBackTitle: "戻る",
-            }}
-          />
-          <Stack.Screen
-            name="others/tip/index"
-            options={{
-              title: "Buy Me a Coffee",
-              headerStyle: { backgroundColor: "#1A1D21" },
-              headerTintColor: "#1E90FF",
-              headerBackTitle: "戻る",
-            }}
-          />
-          <Stack.Screen
-            name="others/external-linkage/index"
-            options={{
-              title: "外部サービス連携",
-              headerStyle: { backgroundColor: "#1A1D21" },
-              headerTintColor: "#1E90FF",
-              headerBackTitle: "戻る",
-            }}
-          />
-        </Stack>
+        <BgmProvider>
+          <StatusBar style="light" />
+          <Stack>
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                title: "ホーム",
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="setup/index"
+              options={{
+                title: "ゲーム設定",
+                headerStyle: { backgroundColor: "#1A1D21" },
+                headerTintColor: "#1E90FF",
+              }}
+            />
+            <Stack.Screen name="play/index" options={{ headerShown: false }} />
+            <Stack.Screen name="result/index" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="how-to-play/index"
+              options={{
+                title: "あそびかた",
+                headerStyle: { backgroundColor: "#1A1D21" },
+                headerTintColor: "#1E90FF",
+              }}
+            />
+            <Stack.Screen
+              name="others/volume/index"
+              options={{
+                title: "音量設定",
+                headerStyle: { backgroundColor: "#1A1D21" },
+                headerTintColor: "#1E90FF",
+                headerBackTitle: "戻る",
+              }}
+            />
+            <Stack.Screen
+              name="others/plans/index"
+              options={{
+                title: "アップグレード",
+                headerStyle: { backgroundColor: "#1A1D21" },
+                headerTintColor: "#1E90FF",
+                headerBackTitle: "戻る",
+              }}
+            />
+            <Stack.Screen
+              name="others/tip/index"
+              options={{
+                title: "Buy Me a Coffee",
+                headerStyle: { backgroundColor: "#1A1D21" },
+                headerTintColor: "#1E90FF",
+                headerBackTitle: "戻る",
+              }}
+            />
+            <Stack.Screen
+              name="others/external-linkage/index"
+              options={{
+                title: "外部サービス連携",
+                headerStyle: { backgroundColor: "#1A1D21" },
+                headerTintColor: "#1E90FF",
+                headerBackTitle: "戻る",
+              }}
+            />
+          </Stack>
+        </BgmProvider>
       </PortalProvider>
     </TamaguiProvider>
   );
